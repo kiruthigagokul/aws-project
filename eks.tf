23addc26d20e66bc1cd45c20928da704c30cca0c
kiruthigagokul <kiruthigasarasulingam@gmail.com>
2026-02-25 14:51:49 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,44 +1,36 @@
+
 module "eks" {
-    source = "terraform-aws-modules/eks/aws"
-    version = "~>20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~>20.0"
 
-    cluster_name                   = local.name
-    cluster_version = "1.30"
-    cluster_endpoint_public_access = true
+  cluster_name    = local.name
+  cluster_version = "1.30"
+  cluster_endpoint_public_access = true
 
-    cluster_addons = {
-        coredns = {
-            most_recent = true
-        }
-        kube_proxy = {
-            most_recent = true
-        }
-        vpc_cni = {
-            most_recent = true
-        }
-    }
-    vpc_id = module.vpc.vpc_id
-    subnet_ids = module.vpc.private_subnets
-    control_plane_subnet_ids = module.vpc.intra_subnets
+  cluster_addons = {
+    coredns = { most_recent = true }
+    kube_proxy = { most_recent = true }
+    vpc_cni = { most_recent = true }
+  }
 
-    eks_managed_node_group_defaults = {
-        ami_type = "AL2023_x86_64_STANDARD"
-        instance_types = ["t2.medium"]
-        attach_cluster_primary_security_group = true
-    }
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
-    eks_managed_node_groups = {
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2023_x86_64_STANDARD"
+    attach_cluster_primary_security_group = true
+  }
+
+  eks_managed_node_groups = {
     gk_worker_node = {
       desired_size   = 1
       max_size       = 2
       min_size       = 1
-      instance_types = ["t2.medium"]
-      capacity_type = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
     }
   }
+
   tags = local.tags
 }
