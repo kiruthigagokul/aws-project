13bb4e60a9315574c9f8eed18e0fb585c5074832
kiruthigagokul <kiruthigasarasulingam@gmail.com>
2026-02-25 15:38:54 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -7,11 +7,7 @@ module "eks" {
   cluster_version = "1.30"
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = { addon_version = "v1.11.4-eksbuild.28" }
-    kube_proxy = { addon_version = "v1.30.14-eksbuild.20" }
-    vpc_cni = { addon_version = "v1.21.1-eksbuild.3" }
-  }
+  cluster_addons = {}
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
