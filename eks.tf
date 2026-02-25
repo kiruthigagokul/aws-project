b67686b05c0446c6c3ff8bd0d69ae10c2d622fee
kiruthigagokul <kiruthigasarasulingam@gmail.com>
2026-02-25 14:42:43 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ module "eks" {
     version = "~>20.0"
 
     cluster_name                   = local.name
-    cluster_version = "1.35"
+    cluster_version = "1.30"
     cluster_endpoint_public_access = true
 
     cluster_addons = {
